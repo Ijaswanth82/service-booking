5356d2055753b0a55bf6aea8d79b456f2180aee6
jinukond <jaswanth.inukonda@here.com>
2025-02-23 19:54:13 +0530

    Initial commit

---
+++ src/main/java/com/example/serviceBook/ServiceBookApplication.java
@@ -0,0 +1,14 @@
+package com.example.serviceBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServiceBookApplication.class, args);
+	}
+
+}
